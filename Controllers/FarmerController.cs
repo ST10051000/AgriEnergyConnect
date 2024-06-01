99eca38dd030d3d30d7f4ce7f0edb2d24e2700d8
Courtney Wicomb <ST10051000@vcconnect.edu.za>
2024-06-01 03:02:03 +0200

    Add project files.

---
+++ Controllers/FarmerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriEnergyConnectPlatform.Controllers
+{
+    [Authorize(Roles ="Employee")]
+    public class FarmerController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using AgriEnergyConnectPlatform.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriEnergyConnectPlatform.Controllers
+{
+    [Authorize(Roles = "Farmer")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+    }
+}
